5d8aa94980ccdd8d6b3b585a811b5f06221d3e58
Pakorn <Pakorn.Thanaprachanon@outlook.com>
2022-03-16 00:56:22 +0700

    Finish scrape raw HTML

--- selenium/src/see_more_script.py
+++ selenium/src/see_more_script.py
@@ -1,38 +1,22 @@
 from .selenium_driver import SeleniumDriver
-import time
 
 
 class SeeMoreScript(SeleniumDriver):
-    _loop = 10
+    _loop = 10  # Number of loop for actual script
+    name = "no_name"  # Name of this target for save resource path
 
-    def __init__(self, url, pre_script_xpath_target, actual_script_xpath_target):
+    def __init__(self, name):
         super().__init__()
-
-        self._browser.get(url)
-        time.sleep(5)
-        self.set_window_size_all_the_way()
-
-        if pre_script_xpath_target:
-            self.pre_script(pre_script_xpath_target)
-        if actual_script_xpath_target:
-            self.actual_script(actual_script_xpath_target)
+        self.name = name
 
     def set_loop(self, new_loop):
         self._loop = new_loop
 
-    def pre_script(self, xpath_target):
+    def pre_script(self):  # Pre-scirpt
         return
 
-    def actual_script(self, xpath_target):  # Actual loop scirpt
-        for x in range(self._loop):
-            print(x)
-            try:
-                self.wait_element_to_load(xpath_target[0])
-                self.wait_to_load_and_click(xpath_target[0])
-            except:
-                print("error")
-                break
-        time.sleep(3)
-        self.fullpage_screenshot("coingecko_final.png")
+    def actual_script(self):  # Actual loop scirpt
+        return
 
-        # def post_script(self):
+    def post_script(self):  # Post-scirpt
+        return
--- selenium/src/selenium_driver.py
+++ selenium/src/selenium_driver.py
@@ -7,11 +7,11 @@ from selenium.common.exceptions import TimeoutException
 
 
 class SeleniumDriver():
-    _browser = None
-    _screenshot_path = "./screenshots/"
-    _result_path = "./results/"
-    _screen_shot = True
-    _delay = 10
+    _browser = None  # Instance driver
+    _screenshot_path = "./screenshots/"  # Directory for screenshot
+    _result_path = "./results/"  # Directory for raw data
+    _screen_shot = True  # Default screenshot option
+    _delay = 10  # Default delay
 
     def __init__(self):
         self._browser = self.set_up()
@@ -38,31 +38,44 @@ class SeleniumDriver():
             os.getenv('REMOTE_SELENIUM_URL', 'http://selenium:4444'), options=chrome_options)
         return browser
 
-    def tear_down(self):
+    def tear_down(self):  # To shut down browser
         self._browser.quit()
 
-    def wait_element_to_load(self, xpath):
+    def get_element(self, xpath):  # Get element by XPATH
+        target_element = WebDriverWait(self._browser, self._delay).until(
+            EC.presence_of_element_located((By.XPATH, xpath)))
+        return target_element
+
+    def wait_element_to_load(self, xpath):  # Wait element to load by XPATH
         try:
             WebDriverWait(self.
                           _browser, self._delay).until(EC.presence_of_element_located((By.XPATH, xpath)))
-            print("Element is ready!")
         except TimeoutException:
             print("Element: " + xpath + " loading took too much time!")
 
+    # Set window size to fit entire html element
     def set_window_size_all_the_way(self):
         def S(X): return self._browser.execute_script(
             'return document.body.parentNode.scroll'+X)
         self._browser.set_window_size(S('Width'), S('Height'))
 
+    # Take screenshot of entire body html
     def fullpage_screenshot(self, name):
         if(self._screen_shot):
             self.set_window_size_all_the_way()
             self._browser.find_element_by_tag_name(
                 'body').screenshot(self._screenshot_path+name)
 
+    # Wait element to load and click by XPATH
     def wait_to_load_and_click(self, xpath):
         try:
             WebDriverWait(self._browser, self._delay).until(EC.element_to_be_clickable(
                 (By.XPATH, xpath))).click()
         except:
             print("Unable to click element: " + xpath)
+
+    # Export text to .txt in a single line
+    def write_to_txt(self, text):
+        new_text = ''.join([line.strip() for line in text])
+        with open(self._result_path + self.name + '.txt', 'w') as f:
+            f.write(new_text)
