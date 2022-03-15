4a2e1cbb5d24abe94512fdfc787d7c4d4153d973
Pakorn <Pakorn.Thanaprachanon@outlook.com>
2022-03-15 14:00:20 +0700

    Add fullscreen_shot

--- selenium/main.py
+++ selenium/main.py
@@ -1,6 +1,15 @@
+from src.selenium_driver import SeleniumDriver
 import time
 
-time.sleep(5)  # Sleep for 5 seconds
-print("START SETUP SELENIUM")
+if __name__ == '__main__':
+    time.sleep(3)  # Sleep for 3 seconds
+    print("START SETUP SELENIUM")
+    chrome_driver = SeleniumDriver(
+        screenshot_path="./screenshots/", result_path="./results/")
+    chrome = chrome_driver.browser
 
-print("FINISH SETUP SELENIUM")
+    chrome.get("https://selenium.dev")
+    chrome_driver.fullpage_screenshot("test1.png")
+
+    chrome_driver.tear_down()
+    print("FINISH SETUP SELENIUM")
--- selenium/src/selenium_driver.py
+++ selenium/src/selenium_driver.py
@@ -1,18 +1,39 @@
 import os
+import time
 from selenium import webdriver
 
 
 class SeleniumDriver():
+    browser = None
+    screenshot_path = ""
+    result_path = ""
 
-    def __init__(self):
+    def __init__(self, screenshot_path, result_path):
+        self.browser = self.set_up()
+        self.screenshot_path = screenshot_path
+        self.result_path = result_path
+
+    def set_up(self):
         # Specifying argument as you launch your browser
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument('--headless')
         chrome_options.add_argument('--incognito')
         chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument('--ignore-certificate-errors')
+        chrome_options.add_argument('--start-maximized')
 
         # Create new Instance of Chrome
         browser = webdriver.Remote(
             os.getenv('REMOTE_SELENIUM_URL', 'http://selenium:4444'), options=chrome_options)
-        self.driver = browser
-        return self.driver
+        browser.implicitly_wait(10)
+        return browser
+
+    def tear_down(self):
+        self.browser.quit()
+
+    def fullpage_screenshot(self, name):
+        def S(X): return self.browser.execute_script(
+            'return document.body.parentNode.scroll'+X)
+        self.browser.set_window_size(S('Width'), S('Height'))
+        self.browser.find_element_by_tag_name(
+            'body').screenshot(self.screenshot_path+name)
