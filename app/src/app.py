62ce5ff5d3b5a3ef6a1c032605000561d0a5f887
Kenta420 <poomipat002@gmail.com>
2022-04-27 00:20:26 +0700

    Done backend (CSV in twilight zone)

--- app/src/app.py
+++ app/src/app.py
@@ -1,134 +1,94 @@
-import os
-import logging
-import asyncio
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect
-import aioredis
-from aioredis.client import PubSub, Redis
-from .notifier import Notifier
-from fastapi.responses import HTMLResponse
-
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-
-redis_url = os.getenv('REDIS_URL')
-redis_port = os.getenv('REDIS_PORT')
-redis_password = os.getenv('REDIS_PASS')
-
+from fastapi import FastAPI
+import pandas as pd
+import re
 
 app = FastAPI()
-notifier = Notifier()
 
-html = """
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Test Websocket</title>
-  </head>
-  <body>
-    <h1>Request Subscript</h1>
-    <form action="" onsubmit="reqMessage(event)">
-        <input type="text" id="reqText" autocomplete="off"/>
-        <button>Send</button>
-    </form>
-    <h1>Revoke Subscript</h1>
-    <form action="" onsubmit="revokeMessage(event)">
-        <input type="text" id="revokeTaxt" autocomplete="off"/>
-        <button>Send</button>
-    </form>
-    <ul id='messages'>
-    <script type="text/javascript">
-      const ws = new WebSocket("ws://localhost:5000/ws");
-      const messages = document.getElementById('messages')
-      ws.addEventListener('open', function (event){
-        ws.send(JSON.stringify({"request": ["BTC", "ETH"]}));
-      });
-      ws.addEventListener('message', function (event){
-        console.log(event.data);
-        const message = document.createElement('li')
-        const content = document.createTextNode(event.data)
-        message.appendChild(content)
-        messages.appendChild(message)
-      });
-      function reqMessage(event) {
-                var input = document.getElementById("reqText")
-                ws.send(JSON.stringify({"request": [input.value]}))
-                input.value = ''
-                event.preventDefault()
-        }
-        function revokeMessage(event) {
-                var input = document.getElementById("revokeTaxt")
-                ws.send(JSON.stringify({"revoke": [input.value]}))
-                input.value = ''
-                event.preventDefault()
-        }
-    </script>
-  </body>
-</html>
-"""
+# test_str = ("BTC/USDT\n"
+#             "DOGE/USDT\n"
+#             "ETH/USDT\n"
+#             "APE/USDT\n"
+#             "BUSD/USDT\n"
+#             "GMT/USDT\n"
+#             "LUNA/USDT\n"
+#             "BNB/USDT\n"
+#             "SHIB/USDT\n"
+#             "NEAR/USDT\n"
+#             "UST/USDT\n"
+#             "XRP/USDT\n"
+#             "SOL/USDT\n"
+#             "USDC/USDT\n"
+#             "JASMY/USDT\n"
+#             "ROSE/USDT\n"
+#             "ADA/USDT\n"
+#             "AVAX/USDT\n"
+#             "RUNE/USDT\n"
+#             "DOT/USDT\n"
+#             "SLP/USDT\n"
+#             "FTM/USDT\n"
+#             "ZIL/USDT\n"
+#             "CAKE/USDT\n"
+#             "TRX/USDT\n"
+#             "KNC/USDT\n"
+#             "SAND/USDT\n"
+#             "MATIC/USDT\n"
+#             "GALA/USDT\n"
+#             "GRT/USDT\n"
+#             "WAVES/USDT\n"
+#             "ATOM/USDT\n"
+#             "ZRX/USDT\n"
+#             "KAVA/USDT\n"
+#             "EOS/USDT\n"
+#             "FIL/USDT\n"
+#             "LINK/USDT\n"
+#             "AAVE/USDT\n"
+#             "XMR/USDT\n"
+#             "EUR/USDT\n"
+#             "VET/USDT\n"
+#             "ETC/USDT\n"
+#             "MANA/USDT\n"
+#             "LTC/USDT\n"
+#             "AXS/USDT\n"
+#             "CRV/USDT\n"
+#             "SNX/USDT\n"
+#             "ENS/USDT\n"
+#             "GLMR/USDT\n"
+#             "DAR/USDT")
 
-
-@app.get("/")
-async def get():
-    return HTMLResponse(html)
-
-STOPWORD = "END"
+filenames = ["bn", "bm", "g", "kc"]
 
 
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
-    await notifier.connect(websocket)
-    await redis_connector(websocket)
+def get_name_currency_list():
+    names = {}
+    for filename in filenames:
+        data = pd.read_csv(f"../data/{filename}.csv")
+        list_name = data["name"].tolist()
+    for n in list_name:
+        names[n] = names.get(n, None)
+    return tuple(names.keys())
 
 
-async def redis_connector(websocket: WebSocket):
-    async def consumer_handler(ws: WebSocket):
-        try:
-            while True:
-                message = await ws.receive_json()
-                print(message)
-                if "request" in message:
-                    await notifier.request_room(websocket, message["request"])
-                if "revoke" in message:
-                    await notifier.revoke(ws, message["revoke"])
-        except WebSocketDisconnect as exc:
-            # TODO this needs handling better
-            logger.error(exc)
-
-    async def producer_handler(pubsub: PubSub):
-        await pubsub.psubscribe("channel:*")
-        try:
-            while True:
-                message = await pubsub.get_message(ignore_subscribe_messages=True)
-                if message is not None:
-                    print(f"(Reader) Message Received: {message}")
-                    data = message["data"]
-                    room = message["channel"][len("channel:"):]
-                    await notifier._notify({"data": data, "room": room}, room)
-                    if message["data"] == STOPWORD:
-                        print("(Reader) STOP")
-                        break
-        except Exception as exc:
-            # TODO this needs handling better
-            logger.error(exc)
-
-    conn = await get_redis_pool()
-    pubsub = conn.pubsub()
-
-    consumer_task = consumer_handler(ws=websocket)
-    producer_task = producer_handler(pubsub=pubsub)
-    done, pending = await asyncio.wait(
-        [consumer_task, producer_task], return_when=asyncio.FIRST_COMPLETED,
-    )
-    logger.debug(f"Done task: {done}")
-    for task in pending:
-        logger.debug(f"Canceling task: {task}")
-        task.cancel()
+list_name = get_name_currency_list()
 
 
-async def get_redis_pool():
-    return await aioredis.from_url(f"{redis_url}:{redis_port}", password=redis_password, encoding="utf-8", decode_responses=True)
+def get_data_currency(currency_id: str):
+    data = {}
+    for filename in filenames:
+        df = pd.read_csv(f"../data/{filename}.csv")
+        data[filename] = df.loc[df["name"] ==
+                                f"{currency_id.upper()}/USDT"].to_dict()
+    return data
+
+
+@app.get("/api/suggest")
+async def suggestion(text: str = ""):
+    if len(text) == 0:
+        return "Please type something"
+    matches = re.findall(r".*{0}.*".format(text), list_name,
+                         re.MULTILINE | re.IGNORECASE)
+    return {"suggest": matches}
+
+
+@app.get("/api/currency/{id}")
+async def currency(id: str):
+    return get_data_currency(id)
